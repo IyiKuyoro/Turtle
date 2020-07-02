1fd1dd2ff867b653ff1490d600c19299401aa77c
Opeoluwa Iyi-Kuyoro <opeoluwa.iyikuyoro@gmail.com>
2020-07-02 13:19:28 -0400

    imporve google search

--- StartSearch/searchdata.py
+++ StartSearch/searchdata.py
@@ -2,10 +2,12 @@ import requests
 
 GOOGLE_SEARCH_ENGINE_ID = '010789377440770036764:wrpyd9iz6d8&q'
 
-def get_search_result_by_term_and_country(key, term, country):
+def get_search_result_by_term_and_country(key, term, country, num_of_results, start_at):
     try:
         global GOOGLE_SEARCH_ENGINE_ID
-        url = 'https://www.googleapis.com/customsearch/v1?key={}&cx={}&q={}&cr={}'.format(key, GOOGLE_SEARCH_ENGINE_ID, term, country)
+        url = 'https://www.googleapis.com/customsearch/v1?key={}&cx={}&q={}&cr={}&lr=lang_en&num={}&start={}'.format(
+            key, GOOGLE_SEARCH_ENGINE_ID, term, country, num_of_results, start_at
+            )
         x = requests.get(url)
 
         return x.json()
@@ -14,10 +16,12 @@ def get_search_result_by_term_and_country(key, term, country):
         raise
 
 
-def get_search_result_by_term(key, term):
+def get_search_result_by_term(key, term, num_of_results, start_at):
     try:
         global GOOGLE_SEARCH_ENGINE_ID
-        url = 'https://www.googleapis.com/customsearch/v1?key={}&cx={}&q={}'.format(key, GOOGLE_SEARCH_ENGINE_ID, term)
+        url = 'https://www.googleapis.com/customsearch/v1?key={}&cx={}&q={}&lr=lang_en&num={}&start={}'.format(
+            key, GOOGLE_SEARCH_ENGINE_ID, term, num_of_results, start_at
+            )
         x = requests.get(url)
 
         return x.json()
