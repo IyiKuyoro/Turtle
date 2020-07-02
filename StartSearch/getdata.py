8cc0280054c9372890728b4c73c32e02291328d8
Opeoluwa Iyi-Kuyoro <opeoluwa.iyikuyoro@gmail.com>
2020-07-02 13:19:14 -0400

    imporve data fetching

--- StartSearch/getdata.py
+++ StartSearch/getdata.py
@@ -11,7 +11,7 @@ def _get_service():
         Discovery: The created google sheet service
     """
     try:
-        scopes = ['https://www.googleapis.com/auth/spreadsheets.readonly']
+        scopes = ['https://www.googleapis.com/auth/spreadsheets']
         secrete_file = os.path.join(os.getcwd(), 'secret.json')
 
         credentials = service_account.Credentials.from_service_account_file(secrete_file, scopes=scopes)
@@ -38,7 +38,29 @@ def _get_data_from_google_sheet(sheet_id, fields, doc_range):
     return r.execute()
 
 
-def parse_data(data):
+def update_data_in_google_sheet(sheet_id, doc_range, values):
+    """Update the app meta
+
+    Args:
+        sheet_id (string): The google sheet id
+        doc_range (string): The range to be updated
+        values (list): A list of data to be entered into the cells
+
+    Returns:
+        Dic: The response data gotten from the request
+    """
+    sheet_service = _get_service()
+    value = {
+        "range": doc_range,
+        "majorDimension": "ROWS",
+        "values": values
+    }
+    value_input_option = 'USER_ENTERED'
+    r = sheet_service.spreadsheets().values().update(spreadsheetId=sheet_id, valueInputOption=value_input_option, range=doc_range, body=value)
+    return r.execute()
+
+
+def parse_data(data, meta):
     """Parese the google sheet data into readable data
 
     Args:
@@ -49,12 +71,19 @@ def parse_data(data):
     """
     result = data['values']
 
-    return {
-        'term': [value[0] for index, value in enumerate(result) if index != 0 and value[0] != ''],
-        'country': [value[1] for index, value in enumerate(result) if index != 0 and value[1] != '']
-    }
+    if meta:
+        return {
+            result[0][0]: [value[0] for index, value in enumerate(result) if index != 0 and value[0] != ''],
+            result[0][1]: [value[1] for index, value in enumerate(result) if index != 0 and value[1] != ''],
+            result[0][2]: [value[2] for index, value in enumerate(result) if index != 0 and value[2] != '']
+        }
+    else:
+        return {
+            result[0][0]: [value[0] for index, value in enumerate(result) if index != 0 and value[0] != ''],
+            result[0][1]: [value[1] for index, value in enumerate(result) if index != 0 and value[1] != '']
+        }
 
 
-def get_sheet_contents(sheet_id, fields, doc_range):
+def get_sheet_contents(sheet_id, fields, doc_range, meta):
     contents = _get_data_from_google_sheet(sheet_id, fields, doc_range)
-    return parse_data(contents)
+    return parse_data(contents, meta)
