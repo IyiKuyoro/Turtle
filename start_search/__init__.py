add43c1486a1938884e0c9b45ae2c63748dc78af
Opeoluwa Iyi-Kuyoro <opeoluwa.iyikuyoro@gmail.com>
2020-07-05 20:07:37 -0400

    create helper module

--- start_search/__init__.py
+++ start_search/__init__.py
@@ -3,86 +3,17 @@
 Returns:
     Module: The main module
 """
-import datetime
-import logging
-import math
-import json
 import os
-
-from dominate.tags import body, div, a, h1, p
+import json
+import random
+import logging
+import datetime
 import azure.functions as func
-from .getdata import get_sheet_contents, update_data_in_google_sheet, get_excluded_site_list
-from .searchdata import get_search_result_by_term
 
-
-def generate_email(report):
-    """Generate the email content
-
-    Args:
-        report (dic): A dictionary containing the report
-
-    Returns:
-        string: The HTML text of the email body
-    """
-    email_body = body()
-    header = h1("This weeks search results")
-    email_body.add(header)
-    for _, results in report.items():
-        for result in results:
-            link = a(h1(result['title']))
-            link['href'] = result['link']
-            snippet = p(result['snippet'])
-            item = div(link, snippet)
-            email_body.add(item)
-
-    return email_body
-
-
-def send_email(receiver, content, send_grid_message):
-    """Send an email
-
-    Args:
-        receiver (string): The email address of the recipient
-        content (string): The content of the email
-        send_grid_message (object): The send grid messager
-    """
-    message = {
-        "personalizations": [{
-            "to": [{
-                "email": receiver
-            }]}],
-        "subject": "Alert Me Report for {}".format(datetime.date.today()),
-        "content": [{
-            "type": "text/HTML",
-            "value": content
-            }]}
-
-    send_grid_message.set(json.dumps(message))
-
-
-def remove_duplicates(report):
-    """
-    Remove all duplicate results from report
-
-    Args:
-        report (dic): The report to be sent
-
-    Returns:
-        dic: The filtered report
-    """
-    dic = {}
-    result = {}
-
-    for search_term in report:
-        for item in report[search_term]:
-            if item['title'] not in dic:
-                dic[item['title']] = True
-                if search_term in result:
-                    result[search_term].append(item)
-                else:
-                    result[search_term] = [item]
-
-    return result
+from .helper import generate_email, send_email, \
+                    remove_duplicates, get_country_code, \
+                    get_all_required_data, run_all_google_searches
+from .getdata import update_data_in_google_sheet
 
 def main(mytimer: func.TimerRequest, sendGridMessage: func.Out[str]) -> None:
     """The main function that is triggered when the timmer elapses
@@ -96,71 +27,32 @@ def main(mytimer: func.TimerRequest, sendGridMessage: func.Out[str]) -> None:
     """
     try:
         google_sheet_id = os.environ["GOOGLE_SHEET_ID"]
-        google_data_sheet_range = 'Sheet1!A1:B100'
-        google_meta_sheet_range = 'turtle_meta!A1:C3'
-        google_search_api_key = os.environ["GOOGLE_SEARCH_API_KEY"]
         num_of_results = 3
+        report = []
 
-        report = {}
-
-        contents = get_sheet_contents(
-            google_sheet_id,
-            'values',
-            google_data_sheet_range,
-            False
-            )
-
-        turtle_meta = get_sheet_contents(
-            google_sheet_id,
-            'values',
-            google_meta_sheet_range,
-            True
-            )
-
-        blacklisted_sites = get_excluded_site_list(
-            sheet_id=google_sheet_id,
-            fields='values',
-            doc_range='exclude_site!A1:A100'
-        )
+        contents, turtle_meta, blacklisted_sites = get_all_required_data(google_sheet_id)
 
         last_result = int(turtle_meta['last_result'][0])
         num_of_groups = int(turtle_meta['num_of_groups'][0])
         next_group = int(turtle_meta['next_group'][0])
 
-        # Group countries
-        countries_len = len(contents['Country'])
-        country_div = math.floor(countries_len / num_of_groups)
-        group_countries = []
-        for i in range(num_of_groups):
-            group_countries.append(' '.join(contents['Country'][i*country_div:country_div*(i + 1)]))
-
-        # Run google search against each term
-        for term in contents['Technical Term']:
-            country = group_countries[next_group]
-            search_term = '{} {}'.format(term, country)
-            results = get_search_result_by_term(
-                google_search_api_key, search_term,
-                num_of_results, last_result, blacklisted_sites
-            )
-
-            if int(results['searchInformation']['totalResults']) > 0:
-                def rearange(item):
-                    return {
-                        "title": item['title'],
-                        "link": item['link'],
-                        "snippet": item['snippet'],
-                    }
-                report[search_term] = map(rearange, results['items'])
-
+        report = run_all_google_searches(
+            contents=contents,
+            blacklisted_sites=blacklisted_sites,
+            num_of_groups=num_of_groups,
+            next_group=next_group,
+            last_result=last_result
+        )
         report = remove_duplicates(report)
+        report = random.sample(report, 50)
 
         # Update turtle meta
         update_data_in_google_sheet(
             google_sheet_id,
             'turtle_meta!A2:C2',
-            [[next_group + 1 if next_group < 3 else 0,
+            [[next_group + 1 if next_group < num_of_groups - 1 else 0,
               num_of_groups,
-              last_result + num_of_results if next_group == 3 else last_result]]
+              last_result + num_of_results if next_group == num_of_groups - 1 else last_result]]
         )
 
         # Send the email
---
+++ start_search/helper.py
@@ -0,0 +1,184 @@
+"""
+This module contains helper methods for the main function
+"""
+import os
+import math
+import json
+import datetime
+
+from dominate.tags import body, div, a, h1, p
+from .getdata import get_sheet_contents, get_excluded_site_list
+from .searchdata import get_search_result_by_term
+
+
+def generate_email(report):
+    """Generate the email content
+
+    Args:
+        report (dic): A dictionary containing the report
+
+    Returns:
+        string: The HTML text of the email body
+    """
+    email_body = body()
+    header = h1("This weeks search results")
+    email_body.add(header)
+    for result in report:
+        link = a(h1(result['title']))
+        link['href'] = result['link']
+        snippet = p(result['snippet'])
+        item = div(link, snippet)
+        email_body.add(item)
+
+    return email_body
+
+
+def send_email(receiver, content, send_grid_message):
+    """Send an email
+
+    Args:
+        receiver (string): The email address of the recipient
+        content (string): The content of the email
+        send_grid_message (object): The send grid messager
+    """
+    message = {
+        "personalizations": [{
+            "to": [{
+                "email": receiver
+            }]}],
+        "subject": "Alert Me Report for {}".format(datetime.date.today()),
+        "content": [{
+            "type": "text/HTML",
+            "value": content
+            }]}
+
+    send_grid_message.set(json.dumps(message))
+
+
+def remove_duplicates(report):
+    """
+    Remove all duplicate results from report
+
+    Args:
+        report (dic): The report to be sent
+
+    Returns:
+        dic: The filtered report
+    """
+    dic = {}
+    result = []
+
+    for item in report:
+        if item['title'] not in dic and '.exe' not in item['link']:
+            dic[item['title']] = True
+            result.append(item)
+
+    return result
+
+
+def get_country_code(country):
+    """
+    Get the google country code for a country
+
+    Args:
+        country (string): The country to be searched against
+
+    Returns:
+        string: The country code
+    """
+    file_path = os.path.join(os.getcwd(), 'start_search/country_code.json')
+    with open(file_path,'r') as content:
+        my_dict=json.load(content)
+
+    if country in my_dict:
+        return my_dict[country]
+
+    return ''
+
+
+def get_all_required_data(google_sheet_id):
+    """
+    Get all the required data to effectively run the search
+
+    Args:
+        google_sheet_id (string): The sheet id where data is held
+
+    Returns:
+        tuple: (contents, turtle_meta, blacklisted_sites)
+    """
+    google_data_sheet_range = 'Sheet1!A1:B100'
+    google_meta_sheet_range = 'turtle_meta!A1:C3'
+
+    contents = get_sheet_contents(
+        google_sheet_id,
+        'values',
+        google_data_sheet_range,
+        False
+    )
+
+    turtle_meta = get_sheet_contents(
+        google_sheet_id,
+        'values',
+        google_meta_sheet_range,
+        True
+    )
+
+    blacklisted_sites = get_excluded_site_list(
+        sheet_id=google_sheet_id,
+        fields='values',
+        doc_range='exclude_site!A1:A100'
+    )
+
+    return (contents, turtle_meta, blacklisted_sites)
+
+
+def run_all_google_searches(contents, blacklisted_sites, num_of_groups, next_group, last_result):
+    """
+    Run the search for each pair of terms
+
+    Args:
+        contents (dict): A dictionary containing the Technical Terms and Countries to be searched
+        blacklisted_sites (list): A list of sites that have been blacklisted
+        num_of_groups (int): The number of places to group the countries into
+        next_group (int): The next group of countries to be searched
+        last_result (int): The last set of results returned
+
+    Returns:
+        list: A list of results gotten from all the search
+    """
+    report = []
+    google_search_api_key = os.environ["GOOGLE_SEARCH_API_KEY"]
+
+    # Group countries
+    countries_len = len(contents['Country'])
+    country_div = math.floor(countries_len / num_of_groups)
+
+    start_country_group = next_group * country_div
+    end_country_group = country_div*(next_group + 1)
+    # Run google search against each term
+    for term in contents['Technical Term']:
+        for country in contents['Country'][start_country_group:end_country_group]:
+            country_code = get_country_code(country.lower())
+
+            joined_search_term = '{} {}'.format(term, country)
+            results = get_search_result_by_term(
+                key=google_search_api_key,
+                term=joined_search_term,
+                num_of_results=10,
+                start_at=last_result,
+                excluded_sites=blacklisted_sites,
+                country=country_code
+            )
+
+            # Sort out the data to be displayed
+            if int(results['searchInformation']['totalResults']) > 0:
+                def rearange(item):
+                    return {
+                        "title": item['title'],
+                        "link": item['link'],
+                        "snippet": item['snippet'],
+                    }
+                for item in results['items']:
+                    report.append(rearange(item))
+
+    return report
