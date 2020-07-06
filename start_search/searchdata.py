92227563b4f234ca9f0b25739642aa7e1e44f4e8
Opeoluwa Iyi-Kuyoro <opeoluwa.iyikuyoro@gmail.com>
2020-07-05 20:07:51 -0400

    refactor search data function

--- start_search/searchdata.py
+++ start_search/searchdata.py
@@ -7,33 +7,8 @@ Returns:
 import os
 import requests
 
-def get_search_result_by_term_and_country(key, term, country, num_of_results, start_at):
-    """
-    Searches for a term on google against a country
 
-    Args:
-        key (string): The google search api key
-        term (string): The term to be searched
-        country (string): The country code
-        num_of_results (int): The number of result to pull in this search
-        start_at (int): The page number to start from
-
-    Returns:
-        dic: A dictionary containing the google search response
-    """
-    technical_term = term.split(' ')
-    google_search_engine_id = os.environ["GOOGLE_SEARCH_ENGINE_ID"]
-    url = 'https://www.googleapis.com/customsearch/v1' \
-            '?key={}&cx={}&q={}&cr={}&lr=lang_en&num={}&start={}&exactTerms={}'.format(
-                key, google_search_engine_id, term, country,
-                num_of_results, start_at, technical_term[0]
-            )
-    google_search_response = requests.get(url)
-
-    return google_search_response.json()
-
-
-def get_search_result_by_term(key, term, num_of_results, start_at, excluded_sites):
+def get_search_result_by_term(key, term, num_of_results, start_at, excluded_sites, country=''):
     """Searches for a term on the web
 
     Args:
@@ -41,6 +16,7 @@ def get_search_result_by_term(key, term, num_of_results, start_at, excluded_site
         term (string): The term to be searched
         num_of_results (int): The number of results to pull from the search
         start_at (int): The page number to start from
+        excluded_sites (list): List of sites not to include in results
 
     Returns:
         dic: A dictionary containing the google search response
@@ -57,8 +33,14 @@ def get_search_result_by_term(key, term, num_of_results, start_at, excluded_site
                 q=term, num=num_of_results, start=start_at,
                 exact_terms=technical_term[0], safe='ACTIVE',
                 site_search=excluded_sites, site_search_filter='e',
-                date_restricted='m[6]'
+                date_restricted='m6'
             )
+    if country == '':
+        url += '&cr={country}'.format(country=country)
+
     google_search_response = requests.get(url)
+
+    if google_search_response.status_code != 200:
+        raise Exception('Error on google search', google_search_response.json())
 
     return google_search_response.json()
